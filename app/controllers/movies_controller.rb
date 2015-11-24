2081146b823386b213833287cefa1291664e3b1d
inklin <twstacey@gmail.com>
2015-11-24 01:20:43 +0000

    Initial commit

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,41 @@
+class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to movies_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  protected
+
+  def movie_params
+    params.require(:movie).permit(
+      :title, :director, :runtime_in_minutes, :description, :poster_image_url, :release_date
+    )
+  end
+end
