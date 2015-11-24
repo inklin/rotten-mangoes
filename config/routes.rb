a59533e8e08a3ee3d117cf9b2ea37a212fe91e6e
inklin <twstacey@gmail.com>
2015-11-24 03:34:20 +0000

    Generate users controllers and users/new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :users, only: [:new, :create]
   resources :movies
   # get 'movies',          to: 'movies#index'
   # get 'movies/new',      to: 'movies#new'
