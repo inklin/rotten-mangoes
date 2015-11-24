825c2c1c30f9ad7b50e1cffc43c299399e5288b8
inklin <twstacey@gmail.com>
2015-11-24 19:52:45 +0000

    Route root to movies#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  root to: 'movies#index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
   resources :movies do
